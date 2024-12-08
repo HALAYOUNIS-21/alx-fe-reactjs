56018e9430307dc53dabab1570fef612a7657e41
HALAYOUNIS21 <halayounis.a@gmail.com>
2024-12-08 02:49:02 +0200

    Remove all files from GitHub repository

--- react-router-advanced/src/components/ProfileSettings.jsx
+++
@@ -1,5 +0,0 @@
-const ProfileSettings = () => {
-    return <h3>Profile Settings Section</h3>;
-};
-
-export default ProfileSettings;
--- react-todo/ jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ["@testing-library/jest-dom"],
-    testEnvironment: "jest-environment-jsdom",
-    transform: {
-      "^.+\\.jsx?$": "babel-jest",
-    },
-  };
