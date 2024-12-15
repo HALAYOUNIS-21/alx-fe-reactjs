2101b7914944d113e43b01eba080183c9aa52d6e
HALAYOUNIS21 <halayounis.a@gmail.com>
2024-12-15 23:06:27 +0200

    Prepare application for Vercel deployment

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,6 +3,8 @@ import Search from './components/Search';
 
 function App() {
     return (
+      <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">Hello, Tailwind CSS!</h1>,/</div>,
         <div className="app">
             <h1>GitHub User Search Application</h1>
             <Search />
