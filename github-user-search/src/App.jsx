95212e36494cfb1163ad509ab9e55082ea4a4b93
HALAYOUNIS21 <halayounis.a@gmail.com>
2024-12-15 22:18:55 +0200

    Ensure correct error message for invalid GitHub user search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from './components/Search';
 
 function App() {
     return (
         <div className="app">
             <h1>GitHub User Search Application</h1>
+            <Search />
         </div>
     );
 }
